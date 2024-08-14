aaf5462130aa6eede979e2278028fffc7e60eef2
krassN <krassnet@mail.ru>
2024-08-15 00:42:03 +0300

    Add files via upload

---
+++ Module 3 Practical Task 2 version 2.py
@@ -0,0 +1,17 @@
+suffix = ('.com', '.net', '.ru')
+sender_fix = 'university.help@gmail.com' # прописываем значение, чтобы удобно было в дальнейшем менять отправителя
+def send_email(message, recipient, *, sender = sender_fix):
+    if '@' not in recipient and '@' not in sender or not recipient.endswith(suffix, -4) or not sender.endswith(suffix, -4):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    elif sender == recipient:
+        print('Нельзя отправить письмо самому себе!')
+    elif sender == sender_fix:
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+    else:
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
